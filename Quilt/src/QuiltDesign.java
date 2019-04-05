f734799598356e69262f40ae6018492b59b9ddd3
twichtendahl <traviswichtendahl@gmail.com>
2019-04-04 20:27:50 -0700

    Makes a grid of Grids

--- Quilt/src/QuiltDesign.java
+++ Quilt/src/QuiltDesign.java
@@ -4,16 +4,17 @@ import java.awt.Graphics;
 public class QuiltDesign {
 	
 	public final static int SIZE = 2000;
-	public final static int LAYERS = 4;
+	public final static int ITERATIONS = 3;
+	public final static int ROWS = 4;
 	
-	public int determineBuffer(int containerHeight) {
-		return containerHeight/120;
+	public static int determineBuffer(int containerHeight) {
+		return containerHeight/80;
 	}
 	
-	public Color shiftColor(Color color) {
-		float[] rgb = {0, 0, 0, 0};
-		color.getRGBComponents(rgb);
-		return new Color(rgb[0], rgb[1], rgb[2], (float) (rgb[3] + .075));
+	public static Color redshiftColor(Color color) {
+		float[] rgb = {0, 0, 0};
+		color.getRGBColorComponents(rgb);
+		return new Color((float) (rgb[0] + .07), rgb[1], rgb[2]);
 	}
 	
 	public static void drawBrickRow(Graphics g, Color color, int containerX, int containerY, int buffer, int containerWidth, int containerHeight, int brickCount) {
@@ -35,15 +36,35 @@ public class QuiltDesign {
 		}
 	}
 	
+	public static void drawRowOfGrids(Graphics g, Color color, int containerX, int containerY, int buffer, int containerHeight, int gridCount, int rows) {
+		int gridSize = containerHeight - 2 * buffer;
+		int innerBuffer = determineBuffer(gridSize);
+		for(int i = 0; i < gridCount; i++) {
+			drawBrickGrid(g, color, containerX + (buffer * (i + 1)) + (gridSize * i), containerY + buffer, innerBuffer, gridSize, rows);
+		}
+	}
+	
+	public static void drawGridOfGrids(Graphics g, Color color, int containerX, int containerY, int buffer, int size, int rows) {
+		int rowHeight = size / rows;
+		for(int i = 0; i < rows; i++) {
+			drawRowOfGrids(g, color, containerX, containerY + i * (rowHeight - buffer), buffer, rowHeight, i + 1, rows);
+		}
+	}
+	
 	public static void main(String[] args) {
 		DrawingPanel panel = new DrawingPanel(SIZE, SIZE);
 		panel.setBackground(Color.WHITE);
 		Graphics g = panel.getGraphics();
 		
-		g.setColor(Color.CYAN);
-		g.fillRect(200, 100, 400, 400);
-		drawBrickGrid(g, Color.RED, 200, 100, 8, 400, 6);
+		drawGridOfGrids(g, Color.BLUE, 0, 0, determineBuffer(SIZE), SIZE, ROWS);
+		for(int i = 0; i < ITERATIONS; i++) {
+			
+		}
+		
 		/*
+		for(int i = 0; i < ROWS; i++) {
+			drawRowOfGrids(g, Color.BLUE, 0, i * (400 - determineBuffer(SIZE)), determineBuffer(SIZE), 400, i + 1, ROWS);
+		}	
 		*/
 	}
 
